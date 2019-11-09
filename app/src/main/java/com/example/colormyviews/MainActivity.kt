eeb167d736d075824fece1bc2aa4bae9ac398053
kianhee <n.kianhee99@gmail.com>
2019-11-09 13:41:06 +0800

    02.3: Constraint layout using the Layout Editor -Coding challenge

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Color class colors for the background
             R.id.box_one_text -> view.setBackgroundResource(R.drawable.hulk)
             R.id.box_two_text -> view.setBackgroundResource(R.drawable.captainamerica)
-            R.id.box_three_text -> view.setBackgroundResource(R.drawable.hulk)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.warmachine)
             R.id.box_four_text -> view.setBackgroundResource(R.drawable.spiderman)
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.groot)
             R.id.box_six_text -> view.setBackgroundColor(Color.MAGENTA)
